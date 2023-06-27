4cdcc15a4c09adc0ac74cacb75bab122c69950e9
sara <sara@mbp>
2023-06-27 14:41:58 -0400

    NIRSpec IFU support

---
+++ badass/badass_nirspec.py
@@ -0,0 +1,181 @@
+import astropy.io.fits as fits
+import astropy.units as u
+import pathlib
+import numpy as np
+import sys
+
+ARGO = True
+
+if ARGO:
+    BADASS_DIR = pathlib.Path('/projects/ssatyapa/spectra/sdoan2/jwst/badass/')
+    # BADASS_DIR = pathlib.Path(__file__).resolve().parent.parent.parent.joinpath('badass')
+else:
+    BADASS_DIR = pathlib.Path('/Users/sara/Dropbox/research/bgc/pubs/props/jwst_cycle2')
+
+if BADASS_DIR.exists():
+    sys.path.insert(0, str(BADASS_DIR))
+    import badass_repo.badass as badass
+    import badass_repo.badass_tools.badass_ifu as ifu
+
+OPTIONS_DIR = pathlib.Path(__file__).resolve().parent.joinpath('options')
+
+TRANSPOSE_FLUX = True
+
+# TARGET_FLUX_UNIT = u.erg / u.s / (u.cm**2) / u.um
+TARGET_FLUX_UNIT = 1e-17 * u.erg / u.s / (u.cm**2) / u.AA
+TARGET_WAVE_UNIT = u.AA
+
+
+def prepare_cube(cube_fits, specres, z=0.0, plot=False):
+    hdu = fits.open(cube_fits)
+
+    dataext = 1
+    CUNIT = 'CUNIT3'
+    BUNIT = 'BUNIT'
+    data = np.array((hdu[dataext].data).T, dtype='float32')
+    header = hdu[dataext].header
+    cunit = header[CUNIT]
+    bunit = header[BUNIT]
+
+    CRVAL = 'CRVAL3'
+    CRPIX = 'CRPIX3'
+    CDELT = 'CDELT3'
+    nwave = data.shape[2]
+    wave0 = header[CRVAL] - (header[CRPIX] - 1) * header[CDELT]
+    wave = wave0 + np.arange(nwave)*header[CDELT]
+    wave *= u.Unit(cunit)
+
+    # pxar = header['PIXAR_SR'] * u.sr
+    # tf = (hdu['SCI'].data.T * u.Unit(bunit)) * pxar
+    tf = (hdu['SCI'].data.T * u.Unit(bunit))
+    spec = tf.to(TARGET_FLUX_UNIT, equivalencies=u.spectral_density(wave)).value
+    spec[np.isnan(spec)] = 0.0
+    spec[spec < 0.0] = 0.0
+    if TRANSPOSE_FLUX: spec = spec.T
+
+    err = u.Quantity(hdu['ERR'].data.T, unit=u.Unit(hdu['ERR'].header['BUNIT']))
+    # err *= pxar
+    err = err.to(TARGET_FLUX_UNIT, equivalencies=u.spectral_density(wave)).value
+    if TRANSPOSE_FLUX: err = err.T
+    ivar = 1/(err**2)
+
+    nx, ny, nz = header['NAXIS1'], header['NAXIS2'], header['NAXIS3']
+    ra, dec = hdu[0].header['TARG_RA'], hdu[0].header['TARG_DEC']
+
+    wave = wave.to(TARGET_WAVE_UNIT).value
+
+    if isinstance(specres, dict):
+        if len(specres['R']) == len(wave):
+            specres = specres['R']
+        else:
+            specres = np.interp(wave, specres['wavelength'], specres['R'], left=specres['R'][0], right=specres['R'][-1])
+
+    # TODO: in toml config
+    aperture = [5, 45, 5, 45]
+
+    wave,flux,ivar,mask,fwhm_res,binnum,\
+    npixels,xpixbin,ypixbin,z,dataid,objname = ifu.prepare_ifu(str(cube_fits), z, format='user', ivar=ivar,
+                                                                                  voronoi_binning=False,
+                                                                                  fixed_binning=False,
+                                                                                  fixed_bin_size=2,
+                                                                                  use_and_mask=False,
+                                                                                  nx=nx, ny=ny, nz=nz,
+                                                                                  ra=ra, dec=dec,
+                                                                                  wave=wave, flux=spec,
+                                                                                  specres=specres,
+                                                                                  aperture=aperture
+                                                                                  )
+
+    if plot:
+        ifu.plot_ifu(cube_fits, wave, flux, ivar, mask, binnum, npixels, xpixbin, ypixbin, z, dataid)
+
+
+def run_badass_spaxel(spaxel_dir, options_name):
+    fits_file = list(spaxel_dir.glob('*.fits'))[0]
+    print('Running spaxel fits: %s' % str(fits_file))
+
+    run_dir = spaxel_dir.joinpath('region_5')
+    options_file = OPTIONS_DIR.joinpath(options_name)
+    badass.run_BADASS(fits_file, run_dir=run_dir, options_file=options_file, nprocesses=None, sdss_spec=False, ifu_spec=True)
+
+
+def run_badass(spaxel_dir, options_name, output_name):
+    options_file = OPTIONS_DIR.joinpath(options_name)
+    if not options_file.exists():
+        raise Exception('Could not find options file: %s' % str(options_file))
+
+    # test_line_d = None
+    # if test_line:
+    #     test_line_d = {'bool':True,'line':'na_'+test_line}
+
+    fits_files = []
+    run_dirs = []
+    spaxel_subdirs = list(spaxel_dir.glob('spaxel_*_*'))
+    for spaxel in spaxel_subdirs:
+        rd = spaxel.joinpath(output_name)
+        if rd.exists():
+            continue
+        run_dirs.append(rd)
+        rd.mkdir(exist_ok=True, parents=True)
+        fits_files.append(spaxel.joinpath('%s.fits' % spaxel.name))
+    if not fits_files:
+        return
+
+    print('Running badass on %d spaxels' % len(fits_files))
+    badass.run_BADASS(fits_files, run_dir=run_dirs, options_file=options_file, nprocesses=48, sdss_spec=False, ifu_spec=True)
+
+
+def run_cube_build(cube_file, output_name):
+    _, _ = ifu.reconstruct_ifu(cube_file, output_name)
+    ifu.plot_reconstructed_cube(cube_file.with_suffix('').joinpath('full_cube', output_name), animated=False)
+
+
+
+# def prepare_cube(cube_fits, specres, z=0.0, aperture=None, plot=False):
+#     hdu = fits.open(cube_fits)
+#     sci_hdu = hdu['SCI']
+#     hdr = sci_hdu.header
+#     nx, ny, nz = hdu['SCI'].header['NAXIS1'], hdu['SCI'].header['NAXIS2'], hdu['SCI'].header['NAXIS3']
+#     ra, dec = hdu[0].header['TARG_RA'], hdu[0].header['TARG_DEC']
+
+#     wave_values = np.arange(hdr['NAXIS3'])*hdr['CDELT3']+hdr['CRVAL3']
+#     wave_unit = hdr['CUNIT3']
+#     wave_microns = u.Quantity(wave_values, unit=wave_unit)
+#     wave = wave_microns.to(u.AA).value
+
+#     flux_values = sci_hdu.data.T
+#     flux_unit = u.Unit(hdr['BUNIT'])
+#     flux = u.Quantity(flux_values, unit=flux_unit)
+
+#     pxar = hdr['PIXAR_SR'] * u.sr
+#     flux *= pxar
+
+#     target_flux_unit = 1e-17 * u.erg / u.s / (u.cm**2) / u.AA
+#     flux = flux.to(target_flux_unit, equivalencies=u.spectral_density(wave_microns)).value
+#     flux = flux.T
+
+#     err_values = u.Quantity(hdu['ERR'].data.T, unit=u.Unit(hdu['ERR'].header['BUNIT']))
+#     err_values *= pxar
+#     err_values = err_values.to(target_flux_unit, equivalencies=u.spectral_density(wave_microns)).value
+#     err_values = err_values.T
+#     ivar = 1/(err_values**2)
+
+#     wave,flux,ivar,mask,fwhm_res,binnum,\
+#     npixels,xpixbin,ypixbin,z,dataid,objname = ifu.prepare_ifu(str(cube_fits), z, format='user', ivar=ivar,
+#                                                                                   voronoi_binning=False,
+#                                                                                   fixed_binning=False,
+#                                                                                   fixed_bin_size=2,
+#                                                                                   use_and_mask=False,
+#                                                                                   nx=nx, ny=ny, nz=nz,
+#                                                                                   ra=ra, dec=dec,
+#                                                                                   wave=wave, flux=flux,
+#                                                                                   specres=specres
+#                                                                                   )
+
+#     if plot:
+#         ifu.plot_ifu(cube_fits, wave, flux, ivar, mask, binnum, npixels, xpixbin, ypixbin, z, dataid)
+
+
+
+
+
---
+++ instruments/nirspec_ifu.py
@@ -0,0 +1,294 @@
+import astropy.io.fits as fits
+import astropy.units as u
+from astroquery.mast import Observations
+from datetime import datetime
+import json
+import matplotlib.pyplot as plt
+import multiprocessing as mp
+import numpy as np
+
+import mast.mast as mast
+import pipeline.crds_utils as crds_utils
+import badass.badass_nirspec as badass_nirspec
+from instruments.instrument_common import Instrument, Pipeline, Extractor
+
+import jwst
+from jwst.pipeline.calwebb_spec2 import Spec2Pipeline
+from jwst.pipeline.calwebb_spec3 import Spec3Pipeline
+
+
+STAGE2_ASN = True
+
+
+class NIRSpec_IFU(Instrument):
+
+    def download_all(self):
+        mast.login(self.mast_token)
+        df = mast.get_program_data(str(self.program_id), 'NIRSPEC/IFU')
+
+        # uncal
+        uncal_df = Observations.filter_products(df, calib_level=[1], productSubGroupDescription='UNCAL').to_pandas()
+        obs_fmt = 'jw%05d%03d' % (self.program_id, self.obs)
+        obs_df = uncal_df[uncal_df.obs_id.str.contains(obs_fmt)]
+
+        for file_name in obs_df.productFilename.values:
+            dest = self.pipeline_dir.joinpath(file_name)
+            if dest.exists():
+                continue
+            print('downloading %s' % file_name)
+            mast.download_file(file_name, dest=dest)
+
+        # asn2
+        asn2_df = Observations.filter_products(df, calib_level=[2], productSubGroupDescription='ASN').to_pandas()
+        obs_fmt = 'jw%05d%03d' % (self.program_id, self.obs)
+        obs_df = asn2_df[(asn2_df.obs_id.str.contains(obs_fmt)) & (asn2_df.productFilename.str.contains('spec'))]
+
+        for file_name in obs_df.productFilename.values:
+            dest = self.pipeline_dir.joinpath(file_name)
+            if dest.exists():
+                continue
+            print('downloading %s' % file_name)
+            mast.download_file(file_name, dest=dest)
+
+        # asn3
+        asn3_df = Observations.filter_products(df, calib_level=[3], productSubGroupDescription='ASN').to_pandas()
+        obs_fmt = 'jw%05d-o%03d' % (self.program_id, self.obs)
+        obs_df = asn3_df[asn3_df.obs_id.str.contains(obs_fmt)]
+
+        for file_name in obs_df.productFilename.values:
+            dest = self.pipeline_dir.joinpath(file_name)
+            if dest.exists():
+                continue
+            print('downloading %s' % file_name)
+            mast.download_file(file_name, dest=dest)
+
+
+    def run_pipeline(self):
+        # crds_utils.cache_crds('nirspec')
+
+        pipeline_config = getattr(self, 'pipeline', {})
+        pipeline = NIRSpec_IFU_Pipeline(pipeline_config)
+
+        # pipeline.run_stage1_all(self.pipeline_dir, self.pipeline_dir)
+        # pipeline.run_stage2_all(self.pipeline_dir, self.pipeline_dir)
+        pipeline.run_stage3_all(self.pipeline_dir, self.pipeline_dir)
+        print('Pipeline for {pname} complete!'.format(pname=self.product_name))
+
+
+    def get_cubes(self):
+        # TODO: use product_name once the asn file is updated correctly
+        cubes = list(self.pipeline_dir.glob('*_nirspec_*s3d.fits'))
+        print('Found {n} cubes'.format(n=len(cubes)))
+        for cube in cubes:
+            cube.rename(self.badass_dir.joinpath(cube.name))
+
+        # grab the list this way since we might not have actually moved anything
+        return list(self.badass_dir.glob('*_s3d.fits'))
+        # return list(self.badass_dir.glob('*g140h-f100lp_s3d.fits'))
+
+
+    def extract_1D(self):
+        cubes = self.get_cubes()
+        for cube in cubes:
+            print('Extracting %s' % str(cube))
+            aperture_dir = cube.parent.joinpath(cube.stem + '_extracted', self.extract['aperture_name'])
+            aperture_dir.mkdir(parents=True, exist_ok=True)
+
+            extractor = NIRSpec_IFU_Extractor(cube, aperture_dir.joinpath(cube.stem + '.fits'), ap_r=self.extract['aperture_radius'], redshift=self.redshift)
+            extractor.extract_aperture()
+
+
+    def get_spec_res(self, fits_file):
+        DISP_FILE_FMT = 'jwst_nirspec_%s_disp.fits'
+        grating = fits_file.name.split('_')[-2].split('-')[0]
+        disp_file = self.inst_data_dir.joinpath(DISP_FILE_FMT % grating)
+        if not disp_file.exists():
+            raise Exception('Could not find dispersion file: %s' % str(disp_file))
+
+        hdu = fits.open(disp_file)
+        return dict(wavelength=hdu[1].data['WAVELENGTH'], R=hdu[1].data['R'])
+
+
+    def run_badass(self):
+        if (not hasattr(self, 'badass')) or (not 'target' in self.badass):
+            raise Exception('badass config expected')
+
+        # TODO: fix
+        if self.badass['target'] == '1D':
+            raise Exception('unimplemented')
+        elif self.badass['target'] == 'cube':
+            self.run_badass_cube()
+        else:
+            raise Exception('unknown badass target: %s' % self.badass['target'])
+
+
+    def run_badass_cube(self):
+        cubes = self.get_cubes()
+        output_name = 'cube_run1'
+        for cube in cubes:
+            print('Preparing cube: %s' % str(cube))
+
+            specres = self.get_spec_res(cube)
+            badass_nirspec.prepare_cube(cube, specres, z=self.redshift, plot=True)
+            spaxel_dir = cube.with_suffix('')
+            badass_nirspec.run_badass(spaxel_dir, self.badass['options_file'], output_name)
+            badass_nirspec.run_cube_build(cube, output_name)
+            # badass_nirspec.run_badass_spaxel(spaxel_dir.joinpath('spaxel_21_23'), self.badass['options_file'])
+            # 'spaxel_21_23/spaxel_21_23.fits'
+            # badass_nirspec.run_badass(spaxel_dir, self.badass['options_file'], output_name, test_line=None, fit_reg=None)
+
+
+
+class NIRSpec_IFU_Pipeline(Pipeline):
+    def update_asn(self, asn_temp):
+        data = json.load(open(asn_temp))
+
+        name = data['products'][0]['name']
+        data['code_version'] = jwst.__version__
+        data['version_id'] = datetime.utcnow().strftime('%Y%m%dt%H%M%S')
+        # data['asn_pool'] = ''
+
+        asn_file = asn_temp.parent.joinpath('%s_updated.json' % asn_temp.stem)
+        json.dump(data, open(asn_file, 'w'))
+
+        return asn_file, name
+
+    def run_stage2_single(self, infile, output_dir):
+        print('Processing: {}'.format(str(infile)))
+
+        # stage2_params = getattr(self, 'stage2', None)
+        # step_params = getattr(stage2_params, 'steps', {})
+        # breakpoint()
+
+        if STAGE2_ASN:
+            asn_file, name = self.update_asn(infile)
+
+            out_file = output_dir.joinpath('%s_cal.fits'%name)
+            if out_file.exists():
+                return None
+
+            # crds_config = Spec2Pipeline.get_config_from_reference(str(asn_file))
+            # spec2 = Spec2Pipeline.from_config_section(crds_config)
+
+            # spec2.output_dir = str(output_dir)
+            # spec2.save_results = True
+            # spec2.run(asn_file)
+
+            spec2 = Spec2Pipeline()
+            spec2.save_results = True
+            spec2.output_dir = str(output_dir)
+
+            # for step, params in step_params.items():
+            #     for key, value in params.items():
+            #         setattr(spec2, step)
+
+            spec2.cube_build.weighting = 'emsm'
+
+            result = spec2(str(asn_file))
+            print('spec2 result: %s' % result)
+
+            plt.figure()
+            hdu = fits.open(out_file)
+            plt.imshow(hdu['SCI'].data)
+            plt.colorbar()
+            plt.savefig(output_dir.joinpath('test_plots', out_file.stem + '.png'))
+
+            return None
+
+        # Run stage 2 directly on rate files
+        if output_dir.joinpath(infile.name.replace('rate', 'cal')).exists():
+            return None
+
+        if infile.name in ['jw01335004001_03104_00001_nrs1_rate.fits', 'jw01335004001_03104_00001_nrs2_rate.fits',]:
+            return None
+
+        spec2 = Spec2Pipeline()
+        spec2.output_dir = str(output_dir)
+        spec2.output_file = str(infile.with_suffix(''))
+
+        spec2.run(infile)
+        spec2.suffix = 'spec2'
+        return None
+
+
+    def run_stage2_all(self, indir, output_dir):
+        if STAGE2_ASN:
+            in_files = indir.glob('*_spec2_*_asn.json')
+        else:
+            in_files = indir.glob('*_rate.fits')
+
+        if not self.multiprocess:
+            for in_file in in_files:
+                self.run_stage2_single(in_file, output_dir)
+            return
+
+        args = [(in_file, output_dir) for in_file in in_files]
+        pool = mp.Pool(processes=self.nprocesses, maxtasksperchild=1)
+        pool.starmap(self.run_stage2_single, args, chunksize=1)
+        pool.close()
+        pool.join()
+
+
+    def run_stage3_single(self, asn_temp, out_dir):
+        print('Processing: {}'.format(str(asn_temp)))
+        asn_file, name = self.update_asn(asn_temp)
+
+        # if len(list(out_dir.glob('%s*_s3d.fits'%name))) == 1:
+        #     return None
+
+        crds_config = Spec3Pipeline.get_config_from_reference(str(asn_file))
+        spec3 = Spec3Pipeline.from_config_section(crds_config)
+
+        spec3.outlier_detection.skip = True
+        spec3.extract_1d.center_xy = (29, 26)
+
+        spec3.output_dir = str(out_dir)
+        spec3.save_results = True
+        spec3.run(asn_file)
+        return None
+
+
+    def run_stage3_all(self, indir, output_dir):
+        asn_files = indir.glob('*_spec3_*_asn.json')
+
+        if not self.multiprocess:
+            for asn_file in asn_files:
+                self.run_stage3_single(asn_file, output_dir)
+            return
+
+        args = [(asn_file, output_dir) for asn_file in asn_files]
+        pool = mp.Pool(processes=self.nprocesses, maxtasksperchild=1)
+        pool.starmap(self.run_stage3_single, args, chunksize=1)
+        pool.close()
+        pool.join()
+
+
+class NIRSpec_IFU_Extractor(Extractor):
+    def __init__(self, file, outfile, ap_r='psf', redshift=0.0):
+        super().__init__(file, outfile, ap_r=ap_r, redshift=redshift)
+
+        self.scope_diam = 6.5 * u.m
+
+        hdu = fits.open(self.file)
+        hdr = hdu['SCI'].header
+
+        # Linear wavelength solution in per-channel cubes
+        self.wave = (np.arange(hdr['NAXIS3'])*hdr['CDELT3']+hdr['CRVAL3']) * u.Unit(hdr['CUNIT3'])
+        # pxar = hdr['PIXAR_SR'] * u.sr # pixel area in steradians
+        # self.cube_spec = (hdu['SCI'].data * u.Unit(hdr['BUNIT'])) * pxar
+        # self.cube_spec = (hdu['SCI'].data * u.Unit(hdr['BUNIT'])) * u.sr
+        # self.var = ((hdu['ERR'].data * u.Unit(hdr['BUNIT'])) * pxar) ** 2
+
+        # TARGET_FLUX_UNIT = u.erg / u.s / (u.cm**2) / u.um
+        self.cube_spec = (hdu['SCI'].data * u.Unit(hdr['BUNIT']))
+        # # self.cube_spec = self.cube_spec.to(TARGET_FLUX_UNIT, equivalencies=u.spectral_density(self.wave))
+        self.cube_spec[np.isnan(self.cube_spec)] = 0.0
+        self.cube_spec[self.cube_spec < 0.0] = 0.0
+
+        self.var = ((hdu['ERR'].data * u.Unit(hdr['BUNIT']))) ** 2
+        self.pix_size = (hdr['CDELT1'] * u.Unit(hdr['CUNIT1'])).to(u.arcsec)
+        hdu.close()
+
+
+
+
