eb202ce771cec72a6526f006c20773eadd7f6197
galactic-fireball <sdoan1@gmail.com>
2025-06-23 10:30:35 -0400

    miri cleanup

--- instruments/miri_ifu.py
+++ instruments/miri_ifu.py
@@ -23,22 +23,6 @@ from jwst.pipeline.calwebb_spec2 import Spec2Pipeline
 from jwst.pipeline.calwebb_spec3 import Spec3Pipeline
 
 
-def get_chband_files(files, ch, band):
-    if (ch == '') or (band == ''):
-        return files
-
-    new_files = []
-    for file in files:
-        hdu = fits.open(file)
-        hdr = hdu[0].header
-        if (hdr['CHANNEL'] == ch) and (hdr['BAND'] == band):
-            new_files.append(file)
-        hdu.close()
-
-    return new_files
-
-
-
 class MIRI_IFU(Instrument):
 
     def post_setup(self, context):
@@ -112,14 +96,8 @@ class MIRI_IFU(Instrument):
         # crds_utils.cache_crds('miri')
         self.run_stage1_all(context, args, indir=self.pipeline_sci_dir, outdir=self.pipeline_sci_dir)
         self.run_stage1_all(context, args, indir=self.pipeline_bkgd_dir, outdir=self.pipeline_bkgd_dir)
-
         self.run_stage2_all(context, args, self.pipeline_sci_dir, self.pipeline_bkgd_dir, self.pipeline_sci_dir, self.pipeline_bkgd_dir, skip_cubes=True)
-
-        # self.run_stage2_all(context, args, self.pipeline_sci_dir, self.pipeline_sci_dir, skip_cubes=True)
-        # self.run_stage2_all(context, args, self.pipeline_bkgd_dir, self.pipeline_bkgd_dir, skip_cubes=False)
-
         self.run_stage3(context, args, self.pipeline_sci_dir, self.pipeline_bkgd_dir, context.pipeline_dir)
-
         print('Pipeline for {pname} complete!'.format(pname=context.config.product_name))
 
 
@@ -170,6 +148,8 @@ class MIRI_IFU(Instrument):
         pool.join()
 
 
+    # The following functions for stage 2 and 3 were modified from David Law and Kirsten Larson's jupyter notebook:
+    #   https://github.com/spacetelescope/jwst-pipeline-notebooks/blob/main/notebooks/MIRI/MRS/JWPipeNB-MIRI-MRS.ipynb
     def create_stage2_asn(self, context, rfile, bg_files, cal_files):
         asn = afl.asn_from_list([str(rfile),], rule=DMSLevel2bBase, product_name=context.config.product_name+'_level2')
 
@@ -191,11 +171,6 @@ class MIRI_IFU(Instrument):
             hdu.close()
 
         asn_out = rfile.parent.joinpath('l2asn.json')
-
-        # ac = asn.__class__
-        # breakpoint()
-        # valid = ac.validate(asn)
-        # print(valid)
 
         _,asn_serialized = asn.dump()
         asn_file = open(asn_out, 'w')
@@ -253,9 +228,6 @@ class MIRI_IFU(Instrument):
         return asn_out
 
 
-
-
-
     def run_stage3(self, context, args, sci_input, bk_input, output_dir):
         spec3_steps = [
             'assign_mtwcs', 'master_background', 'outlier_detection', 'mrs_imatch',
@@ -273,7 +245,9 @@ class MIRI_IFU(Instrument):
 
         spec3_dict['pixel_replace']['skip'] = False
         spec3_dict['pixel_replace']['algorithm'] = 'mingrad'
-        spec3_dict['cube_build']['output_type'] = 'channel' #'band'
+        spec3_dict['cube_build']['output_type'] = 'channel' # 'channel', 'band', 'multi'
+        # spec3_dict['cube_build']['list_par1'] = ['1', '1', '1', '2', '2', '2', '3', '3', '3', '4', '4', '4',]
+        # spec3_dict['cube_build']['list_par1'] = ['short', 'medium', 'long', 'short', 'medium', 'long', 'short', 'medium', 'long', 'short', 'medium', 'long',]
         spec3_dict['extract_1d']['ifu_autocen'] = True
 
         sci_cal_files = [str(s) for s in sci_input.glob('*_cal.fits')]
@@ -281,287 +255,4 @@ class MIRI_IFU(Instrument):
 
         asn_file = self.create_stage3_asn(context, sci_cal_files, bg_x1d_files, output_dir)
         Spec3Pipeline.call(asn_file, steps=spec3_dict, save_results=True, output_dir=str(output_dir))
-
-
-
-
-    def run_stage3_old(self, context, args, sci_input, bk_input, output_dir):
-        stage3_opts = args.get('stage3', {})
-        include_bkgd = stage3_opts.get('include_bkgd', True)
-        output_dir.mkdir(exist_ok=True, parents=True)
-
-        sci_files = [str(f) for f in sci_input.glob('*_cal.fits')]
-        # jw02521001001_02101_00001_mirifulong_cal.fits
-        asn = asn_from_list.asn_from_list(sci_files, rule=DMS_Level3_Base, product_name=context.config.product_name)
-        breakpoint()
-
-        # jw02521003001_02101_00003_mirifushort_x1d.fits
-        if include_bkgd:
-            bk_files = bk_input.glob('*_x1d.fits')
-            for bkfile in bk_files:
-                asn['products'][0]['members'].append({'expname': str(bkfile), 'exptype': 'background'})
-
-        asnfile = output_dir.joinpath('%s_asn.json' % context.config.product_name)
-        with open(asnfile, 'w') as afile:
-            afile.write(asn.dump()[1])
-
-        crds_config = Spec3Pipeline.get_config_from_reference(str(asnfile))
-        spec3 = Spec3Pipeline.from_config_section(crds_config)
-
-        spec3.output_dir = str(output_dir)
-        spec3.save_results = True
-        spec3.run(asnfile)
-
-
-    def run_stage3_old2(self, context, args, sci_input, bk_input, output_dir):
-        stage3_opts = args.get('stage3', {})
-        include_bkgd = stage3_opts.get('include_bkgd', True)
-        output_dir.mkdir(exist_ok=True, parents=True)
-
-        for ch in range(1,5):
-            asn = DMS_Level3_Base()
-            product_name = context.config.product_name+'_ch%d'%ch
-
-            sci_files = [str(f) for f in sci_input.glob('*_%05d_mirifu*_cal.fits'%ch)]
-            items = [(str(f), 'science') for f in sci_input.glob('*_%05d_mirifu*_cal.fits'%ch)]
-            # asn._add_items(sci_files, product_name=product_name)
-
-            if include_bkgd:
-                # bk_files = bk_input.glob('*_%05d_mirifu*_x1d.fits')
-                # for bkfile in bk_files:
-                    # asn['products'][-1]['members'].append({'expname': str(bkfile), 'exptype': 'background'})
-
-                bkitems = [(str(f), 'background') for f in bk_input.glob('*_%05d_mirifu*_x1d.fits'%ch)]
-                items += bkitems
-
-            asn._add_items(items, product_name=product_name, with_exptype=True)
-
-            asnfile = output_dir.joinpath('%s_ch%d_asn.json' % (context.config.product_name,ch))
-            with open(asnfile, 'w') as afile:
-                afile.write(asn.dump()[1])
-
-            crds_config = Spec3Pipeline.get_config_from_reference(str(asnfile))
-            spec3 = Spec3Pipeline.from_config_section(crds_config)
-
-            spec3.output_dir = str(output_dir)
-            spec3.save_results = True
-            # breakpoint()
-            spec3.run(asnfile)
-
-
-
-
-
-
-    def get_cubes(self):
-        cubes = list(self.pipeline_dir.glob('*_s3d.fits'))
-        print('Found {n} cubes'.format(n=len(cubes)))
-        for cube in cubes:
-            cube.rename(self.badass_dir.joinpath(cube.name))
-
-        # grab the list this way since we might not have actually moved anything
-        return list(self.badass_dir.glob('*_s3d.fits'))
-
-
-    def extract_1D(self):
-        # TODO: use default config
-        # self.extract = getattr(self, 'extract', {})
-        # self.extract['aperture_name'] = getattr(self.extract, 'aperture_name', 'aperture')
-        # self.extract['aperture_radius'] = getattr(self.extract, 'aperture_radius', 'psf')
-
-        cubes = self.get_cubes()
-        for cube in cubes:
-            aperture_dir = cube.parent.joinpath(cube.stem + '_extracted', self.extract['aperture_name'])
-            aperture_dir.mkdir(parents=True, exist_ok=True)
-
-            extractor = MIRI_IFU_Extractor(cube, aperture_dir.joinpath(cube.stem + '.fits'), ap_r=self.extract['aperture_radius'])
-            extractor.extract_aperture()
-
-
-    def extract_spaxels(self):
-        raise Exception('UNIMPLEMENTED')
-        # cubes = self.get_cubes()
-
-        # for cube in cubes:
-        #     spaxel_dir = cube.with_suffix('')
-        #     if not spaxel_dir.exists():
-        #         channel = int(re.findall(r'_ch\d+-shortmediumlong_s3d', cube.name)[0].split('-')[0][3:])
-        #         specres = badass_miri.get_channel_resolution(channel, 'short')
-        #         badass_miri.prepare_cube(cube, specres, z=self.redshift)
-
-
-    def run_badass(self):
-        if (not hasattr(self, 'badass')) or (not 'target' in self.badass):
-            raise Exception('badass config expected')
-
-        # TODO: fix
-        if self.badass['target'] == '1D':
-            self.run_badass_1D()
-        elif self.badass['target'] == 'cube':
-            self.run_badass_cube()
-        else:
-            raise Exception('unknown badass target: %s' % self.badass['target'])
-
-
-    def run_badass_1D(self):
-        if not 'line' in self.badass:
-            raise Exception('Expected line')
-
-        line = badass_miri.get_line(self.badass['line'])
-
-        extracted_dirs = list(self.badass_dir.glob('%s_ch%d*_extracted' % (self.product_name, line.channel)))
-        if len(extracted_dirs) != 1:
-            raise Exception('Could not find extracted_dir')
-        spec_fits = list(extracted_dirs[0].joinpath(self.extract['aperture_name']).glob('*.fits'))
-        if len(spec_fits) != 1:
-            raise Exception('Could not find spec fits file for channel %d' % line.channel)
-        spec_fits = spec_fits[0]
-
-        specres = badass_miri.get_channel_resolution(line.channel, line.subarray)
-        diff = self.badass['wave_range']/2 if 'wave_range' in self.badass else 2000
-        fit_reg = (line.wave-diff, line.wave+diff)
-
-        badass_miri.run_badass_extracted(spec_fits, self.badass['options_file'], '%s_region'%self.badass['line'], specres, self.redshift, fit_reg=fit_reg)
-
-        # args = [(rfile, output_dir, skip_cubes) for rfile in rate_files]
-        # pool = mp.Pool(processes=self.nprocesses, maxtasksperchild=1)
-        # pool.starmap(self.run_stage2_single, args, chunksize=1)
-        # pool.close()
-        # pool.join()
-
-
-    def combine_results(self):
-        rows = {'line_name':[], 'flux':[]}
-        for rdir in self.badass_dir.glob('%s*_extracted/%s/*/*_region' % (self.product_name, self.extract['aperture_name'])):
-            line = rdir.name.split('_region')[0]
-            var_name = 'na_%s_FLUX'%line
-            par_table = rdir.joinpath('log', 'par_table.fits')
-
-            hdu = fits.open(par_table)
-            data = hdu[1].data
-            flux = data[np.where(data['parameter'] == var_name)[0][0]][1]
-            rows['line_name'].append(line)
-            rows['flux'].append(10**flux if flux != 0.0 else flux)
-
-        pd.DataFrame(rows).to_csv(self.badass_dir.joinpath('line_fluxes.csv'), index=False)
-
-
-    # def get_fluxes(self):
-    #     rows = {'line_name':[], 'flux':[]}
-
-    #     for ch in range(1, 5):
-    #         extract_dir = pathlib.Path('ngc7469_pipeline_1.9.2_ch%d_extract'%ch).resolve()
-    #         for rdir in extract_dir.glob('*_region'):
-    #             line = rdir.name.split('_region')[0]
-    #             var_name = 'na_%s_FLUX'%line
-    #             par_table = rdir.joinpath('log', 'par_table.fits')
-
-    #             hdu = fits.open(par_table)
-    #             data = hdu[1].data
-
-    #             flux = data[np.where(data['parameter'] == var_name)[0][0]][1]
-    #             rows['line_name'].append(line)
-    #             rows['flux'].append(10**flux if flux != 0.0 else flux)
-
-    #     pd.DataFrame(rows).to_csv('line_fluxes.csv', index=False)
-
-
-    # def run_badass_line_region_extracted(self, line_name, options_file):
-    #     self.badass_dir = self.data_dir.joinpath('badass')
-    #     extracted_dir = self.badass_dir.joinpath('extracted')
-    #     if not extracted_dir.exists():
-    #         raise Exception('Extracted directory not found')
-
-    #     line = badass_miri.get_line(line_name)
-
-    #     cube_prefix = self.mast_cube_prefix if (self.product_name == 'mast_direct') else self.product_name
-    #     spec_fits = extracted_dir.joinpath('%s_ch%d_extract.fits' % (cube_prefix, line.channel))
-    #     if not spec_fits.exists():
-    #         raise Exception('Could not find extracted spectrum file: %s' % str(spec_fits))
-
-    #     specres = badass_miri.get_channel_resolution(line.channel, line.subarray)
-
-    #     # TODO: make wavelength range an option
-    #     badass_miri.run_badass_extracted(spec_fits, options_file, '%s_region'%line_name, specres, self.redshift, fit_reg=(line.wave-2000,line.wave+2000))
-
-
-    # def run_badass_line_region(self, line_name, options_file):
-    #     if not hasattr(self, 'badass_dir') or not self.badass_dir.exists():
-    #         raise Exception('Badass directory not set or initialized!')
-
-    #     line = badass_miri.get_line(line_name)
-
-    #     cube_prefix = self.mast_cube_prefix if (self.product_name == 'mast_direct') else self.product_name
-    #     cube_fits = self.badass_dir.joinpath('%s_ch%d-shortmediumlong_s3d.fits' % (cube_prefix, line.channel))
-    #     if not cube_fits.exists():
-    #         raise Exception('Could not find cube fits file: %s' % str(cube_fits))
-
-    #     spaxel_dir = cube_fits.with_suffix('')
-    #     if not spaxel_dir.exists():
-    #         raise Exception('Spaxel directories not initialized (%s)' % (str(spaxel_dir)))
-
-    #     badass_miri.run_badass(spaxel_dir, options_file, '%s_region'%line_name, fit_reg=(line.wave-2000,line.wave+2000))
-
-
-    # def run_cube_reconstruct(self, line_name):
-    #     line = badass_miri.get_line(line_name)
-
-    #     cube_prefix = self.mast_cube_prefix if (self.product_name == 'mast_direct') else self.product_name
-    #     cube_fits = self.badass_dir.joinpath('%s_ch%d-shortmediumlong_s3d.fits' % (cube_prefix, line.channel))
-    #     if not cube_fits.exists():
-    #         raise Exception('Could not find cube fits file: %s' % str(cube_fits))
-
-    #     badass_miri.reconstruct_cube(cube_fits, '%s_region'%line_name)
-    #     badass_miri.plot_cube(cube_fits, '%s_region'%line_name)
-
-
-    # def create_ratio_map(self, line1, line2):
-    #     cube_prefix = self.mast_cube_prefix if (self.product_name == 'mast_direct') else self.product_name
-    #     cube_fits1 = self.badass_dir.joinpath('%s_ch%d-shortmediumlong_s3d.fits' % (cube_prefix, line1.channel))
-    #     cube1 = self.badass_dir.joinpath(cube_fits1.stem, 'full_cube', '%s_region'%line1.name)
-    #     if not cube1.exists():
-    #         raise Exception('Could not find spaxel directory: %s' % str(cube1))
-
-    #     cube_fits2 = self.badass_dir.joinpath('%s_ch%d-shortmediumlong_s3d.fits' % (cube_prefix, line2.channel))
-    #     cube2 = self.badass_dir.joinpath(cube_fits2.stem, 'full_cube', '%s_region'%line2.name)
-    #     if not cube2.exists():
-    #         raise Exception('Could not find spaxel directory: %s' % str(cube2))
-
-    #     plot_dir = self.badass_dir.joinpath('ratio_plots')
-    #     plot_dir.mkdir(exist_ok=True, parents=True)
-    #     out_plot = plot_dir.joinpath('%s_vs_%s.pdf' % (line1.name, line2.name))
-
-    #     badass_miri.plot_ratio(cube1, 'NA_%s_FLUX'%line1.name.upper(), cube2, 'NA_%s_FLUX'%line2.name.upper(), out_plot)
-
-
-    # def create_all_ratio_maps(self):
-    #     for i, line1 in enumerate(miri_consts.lines):
-    #         for line2 in miri_consts.lines[i+1:]:
-    #             self.create_ratio_map(line1, line2)
-
-
-class MIRI_IFU_Pipeline(Pipeline):
-    def init(self):
-        super().init()
-        self.residual_fringe_correct = getattr(self, 'residual_fringe_correct', False)
-        self.include_bkgd = getattr(self, 'include_bkgd', True)
-
-
-
-class MIRI_IFU_Extractor(Extractor):
-    def __init__(self, file, outfile, ap_r='psf', plot=True, redshift=0.0):
-        super().__init__(file, outfile, ap_r=ap_r, plot=plot, redshift=redshift)
-
-        self.scope_diam = 6.5 * u.m
-
-        hdu = fits.open(self.file)
-        hdr = hdu['SCI'].header
-
-        # Linear wavelength solution in per-channel cubes
-        self.wave = (np.arange(hdr['NAXIS3'])*hdr['CDELT3']+hdr['CRVAL3']) * u.Unit(hdr['CUNIT3'])
-        pxar = hdr['PIXAR_SR'] * u.sr # pixel area in steradians
-        self.cube_spec = (hdu['SCI'].data * u.Unit(hdr['BUNIT'])) * pxar
-        self.var = ((hdu['ERR'].data * u.Unit(hdr['BUNIT'])) * pxar) ** 2
-        self.pix_size = (hdr['CDELT1'] * u.Unit(hdr['CUNIT1'])).to(u.arcsec)
-        hdu.close()
 
